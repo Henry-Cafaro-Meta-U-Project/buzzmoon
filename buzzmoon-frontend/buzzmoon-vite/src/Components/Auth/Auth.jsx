827c9d688214e812408c6665d4e56ea47c07910f
Henry Cafaro <hjcafaro@fb.com>
2022-06-28 13:04:47 -0700

    styling login page

--- buzzmoon-frontend/buzzmoon-vite/src/Components/Auth/Auth.jsx
+++ buzzmoon-frontend/buzzmoon-vite/src/Components/Auth/Auth.jsx
@@ -16,14 +16,25 @@ export default function Auth(props) {
 
   return (
     <div className='auth'>
+      
       <form className='auth-form'>
-        <input type="text" name="username" value={username} 
-          onChange={(event) => {setUsername(event.target.value)}}></input>
-        <input type="password" name="password" value={password} 
-          onChange={(event) => {setPassword(event.target.value)}}></input>  
-        <button type="button" onClick={handleLogin}>Log In</button>
+        <div className='flex-form'>
+          <li>
+            <label for="username">Username </label>
+            <input type="text" name="username" value={username} 
+              onChange={(event) => {setUsername(event.target.value)}}>
+            </input>
+          </li>
+          <li>
+            <label for="password">Password </label>
+            <input type="password" name="password" value={password} 
+              onChange={(event) => {setPassword(event.target.value)}}></input>  
+          </li>
+          <li>
+            <button type="button" onClick={handleLogin}>Log In</button>
+          </li>
+        </div>
       </form>
-
     </div>
   )
 
