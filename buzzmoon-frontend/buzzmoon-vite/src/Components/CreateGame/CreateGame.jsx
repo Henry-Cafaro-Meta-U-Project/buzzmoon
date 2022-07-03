effb152a8506ca78a41941ea77846fe8ac2e71da
Henry Cafaro <hjcafaro@fb.com>
2022-07-03 16:07:10 -0700

    linting

--- buzzmoon-frontend/buzzmoon-vite/src/Components/CreateGame/CreateGame.jsx
+++ buzzmoon-frontend/buzzmoon-vite/src/Components/CreateGame/CreateGame.jsx
@@ -9,58 +9,84 @@ let uniqueQuestions = 0;
 const getUniqueKey = () => {
   uniqueQuestions += 1;
   return uniqueQuestions;
-}
+};
 
 export default function CreateGame() {
-  let [title, setTitle] = React.useState("");
-  let [desc, setDesc] = React.useState("");
-  let [questions, setQuestions] = React.useState([]);
-
+  const [title, setTitle] = React.useState('');
+  const [desc, setDesc] = React.useState('');
+  const [questions, setQuestions] = React.useState([]);
 
   const deleteQuestion = (questionNumber) => {
-    setQuestions(questions.filter((e) => (e.number !== questionNumber)).map((e, idx) => ({...e, number:idx+1})));
-  }
+    setQuestions(questions.filter((e) => (e.number !== questionNumber))
+      .map((e, idx) => ({ ...e, number: idx + 1 })));
+  };
 
   const modifyQuestion = (questionNumber, newQuestion) => {
     setQuestions(questions.filter((e) => (e.number !== questionNumber))
-                          .concat([newQuestion])
-                          .sort((a,b) => (a.number < b.number ? -1 : 1)));
-  }
+      .concat([newQuestion])
+      .sort((a, b) => (a.number < b.number ? -1 : 1)));
+  };
 
   return (
-    <div className='create-game'>
-      <div className='game-details'>
+    <div className="create-game">
+      <div className="game-details">
         <h2>Game Info</h2>
-        Title: <input type="text" name="title" placeholder='Enter Title' value={title}
-          onChange={(e) => {setTitle(e.target.value)}}></input> <br />
-        Description: <br />
-        <textarea name="description" placeholder='Enter Description' value={desc}
-          onChange={(e) => setDesc(e.target.value)}></textarea>
+        Title:
+        {' '}
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Title"
+          value={title}
+          onChange={(e) => { setTitle(e.target.value); }}
+        />
+        {' '}
+        <br />
+        Description:
+        {' '}
+        <br />
+        <textarea
+          name="description"
+          placeholder="Enter Description"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        />
       </div>
-      <div className='question-box'>
+      <div className="question-box">
         <h2>Questions</h2>
-        <div className='question-list'>
-          {questions.map((q, ind) => (
-            <CreateQuestion key={q.id} question={q} questions={questions} setQuestions={setQuestions} 
-              deleteQuestion={deleteQuestion} modifyQuestion={modifyQuestion}/>
+        <div className="question-list">
+          {questions.map((q) => (
+            <CreateQuestion
+              key={q.id}
+              question={q}
+              questions={questions}
+              setQuestions={setQuestions}
+              deleteQuestion={deleteQuestion}
+              modifyQuestion={modifyQuestion}
+            />
           ))}
         </div>
-        <button type="button" className='add-question-button' onClick={() => {
-          setQuestions(questions.concat(
-            [
-              {
-              id:getUniqueKey(),
-              number:questions.length+1,
-              answers:"",
-              audioFile:null
-              }
-            ]));
-        }}>New Question</button>
-        
-      </div>
-      <div className='submit-details'>
+        <button
+          type="button"
+          className="add-question-button"
+          onClick={() => {
+            setQuestions(questions.concat(
+              [
+                {
+                  id: getUniqueKey(),
+                  number: questions.length + 1,
+                  answers: '',
+                  audioFile: null,
+                },
+              ],
+            ));
+          }}
+        >
+          New Question
+        </button>
 
       </div>
+      <div className="submit-details" />
     </div>
   );
 }
