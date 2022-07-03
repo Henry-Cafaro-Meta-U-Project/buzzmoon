2cbc7069c3330a5b2498495e9e0d7777981f1d7d
Henry Cafaro <hjcafaro@fb.com>
2022-07-03 15:56:16 -0700

    added audio playback of uploaded audio in create-game-view

--- buzzmoon-frontend/buzzmoon-vite/src/Components/CreateQuestion/CreateQuestion.jsx
+++ buzzmoon-frontend/buzzmoon-vite/src/Components/CreateQuestion/CreateQuestion.jsx
@@ -2,9 +2,26 @@ import * as React from 'react';
 import './CreateQuestion.css';
 
 export default function CreateQuestion(props) {
+  let [questionAudio, setQuestionAudio] = React.useState();
+  let [playingMode, setPlayingMode] = React.useState("no audio");
+  // options are "no audio", "playing", "paused"
+
+  
+
+  React.useEffect(() => {
+    if(props.question.audioFile) {
+      const newAudio = new Audio(URL.createObjectURL(props.question.audioFile));
+      newAudio.onended = () => {setPlayingMode("paused")};
+      setQuestionAudio(newAudio);
+      //console.log('URL.createObjectURL(props.question.audioFile: ', URL.createObjectURL(props.question.audioFile));
+      //questionAudio.play();
+    }
+
+  }, [props.question.audioFile])
   
 
   const handleUploadFile = (e) => {
+    setPlayingMode("paused");
     props.modifyQuestion(props.question.number, {...props.question, audioFile:(e.target.files[0])});
   }
 
@@ -15,7 +32,27 @@ export default function CreateQuestion(props) {
           Question #{props.question.number} 
         </div>
         <div className='create-question-header-center'>
-          <input type='file' name='audio-file' accept=".mp3, .m4a" onChange={handleUploadFile}></input>
+          <div className='play-pause-widget'>
+              {questionAudio && 
+                (playingMode == "paused" ? 
+                  <button type="button" className='play-audio' 
+                    onClick={() => {
+                      setPlayingMode("playing");
+                      questionAudio.play()}}>
+                    <i className="fa-solid fa-play"></i>
+                  </button>
+                  :
+                  <button type="button" className='paused-audio' 
+                    onClick={() => {
+                      setPlayingMode("paused");
+                      questionAudio.pause()}}>
+                    <i className="fa-solid fa-pause"></i>
+                  </button>
+                )}
+            </div>
+          <div className='upload-audio-widget'>
+            <input type='file' name='audio-file' accept=".mp3, .m4a" onChange={handleUploadFile} title="Choose an .mp3 or .m4a"></input>
+          </div>
         </div>
         <div className='create-question-header-right'>
           <button type="button" name="delete-question-button" onClick={() => {
