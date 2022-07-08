33e5d27b8d49ba293724e1b898f8ef463f21fcec
Henry Cafaro <hjcafaro@fb.com>
2022-07-08 15:55:58 -0700

    restyled signup card with Chakra

--- buzzmoon-frontend/buzzmoon-vite/src/Components/Auth/Auth.jsx
+++ buzzmoon-frontend/buzzmoon-vite/src/Components/Auth/Auth.jsx
@@ -31,81 +31,12 @@ export default function Auth(props) {
         </button>
       </div>
       
-      {/* {mode === "login" && <LoginForm setCurrentUser={props.setCurrentUser}/>} */}
       {mode === "login" && <LoginCard setCurrentUser={props.setCurrentUser}/>}
-      {mode === "signup" && <SignupForm setCurrentUser={props.setCurrentUser}/>}
+      {mode === "signup" && <SignUpCard setCurrentUser={props.setCurrentUser}/>}
 
     </div>
   )
 }
-
-function LoginForm(props) {
-  let [email, setEmail] = React.useState('');
-  let [password, setPassword] = React.useState('');
-
-  const handleLogin = async () => {
-    await BackendActor.handleLogin(email, password, props.setCurrentUser);
-  }
-
-  return (
-    <form className='auth-form'>
-        <div className='flex-form'>
-          <li>
-            <label htmlFor="email">Email </label>
-            <input type="text" name="email" value={email} 
-              onChange={(event) => {setEmail(event.target.value)}}>
-            </input>
-          </li>
-          <li>
-            <label htmlFor="password">Password </label>
-            <input type="password" name="password" value={password} 
-              onChange={(event) => {setPassword(event.target.value)}}></input>  
-          </li>
-          <li>
-            <button type="button" onClick={handleLogin}>Log In</button>
-          </li>
-        </div>
-      </form>
-  );
-}
-
-function SignupForm(props) {
-  let [email, setEmail] = React.useState('');
-  let [password, setPassword] = React.useState('');
-  let [realName, setRealName] = React.useState('');
-
-  const handleSignup = async () => {
-    await BackendActor.handleSignup(email, password, realName, props.setCurrentUser);
-  }
-
-  return (
-    <form className='auth-form'>
-        <div className='flex-form'>
-          <li>
-            <label htmlFor="email">Email </label>
-            <input type="text" name="email" value={email} 
-              onChange={(event) => {setEmail(event.target.value)}}>
-            </input>
-          </li>
-          <li>
-            <label htmlFor="password">Password </label>
-            <input type="password" name="password" value={password} 
-              onChange={(event) => {setPassword(event.target.value)}}></input>  
-          </li>
-          <li>
-            <label htmlFor="real name">Real Name </label>
-              <input type="realName" name="real name" value={realName} 
-                onChange={(event) => {setRealName(event.target.value)}}></input>  
-          </li>
-          <li>
-            <button type="button" onClick={handleSignup}>Sign Up</button>
-          </li>
-        </div>
-      </form>
-  );
-}
-
-
 
 function LoginCard(props) {
   let [email, setEmail] = React.useState('');
@@ -157,6 +88,73 @@ function LoginCard(props) {
                 }}
                 type="submit">
                 Sign in
+              </Button>
+            </Stack>
+            </form>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
+
+function SignUpCard(props) {
+  let [email, setEmail] = React.useState('');
+  let [password, setPassword] = React.useState('');
+  let [realName, setRealName] = React.useState('');
+
+  const handleSignup = async () => {
+    await BackendActor.handleSignup(email, password, realName, props.setCurrentUser);
+  }
+
+  return (
+    <Flex
+      minH={'50vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Create your account</Heading>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              handleSignup();
+            }}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input 
+                type="email" 
+                onChange={(event) => {setEmail(event.target.value)}}/>
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <Input 
+                type="password" 
+                onChange={(event) => (setPassword(event.target.value))}/>
+            </FormControl>
+            <FormControl id="realName" isRequired>
+              <FormLabel>Real Name</FormLabel>
+              <Input 
+                type="text" 
+                onChange={(event) => (setRealName(event.target.value))}/>
+            </FormControl>
+            <Stack spacing={10} mt={5}>
+              <Button
+                bg={'blue.400'}
+                color={'black'}
+                _hover={{
+                  bg: 'blue.500',
+                  color: 'white',
+                }}
+                type="submit">
+                Sign up
               </Button>
             </Stack>
             </form>
