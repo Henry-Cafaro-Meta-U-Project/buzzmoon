aecd810d532f65d877b4db3e6cd15c11f6dcd661
Henry Cafaro <hjcafaro@fb.com>
2022-07-08 15:50:08 -0700

    restyled login card with Chakra

--- buzzmoon-frontend/buzzmoon-vite/src/Components/Auth/Auth.jsx
+++ buzzmoon-frontend/buzzmoon-vite/src/Components/Auth/Auth.jsx
@@ -2,6 +2,21 @@ import * as React from 'react';
 import BackendActor from '../BackendActor/backend-actor';
 import './Auth.css';
 
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
 export default function Auth(props) {
   let [mode, setMode] = React.useState("login");
 
@@ -16,7 +31,8 @@ export default function Auth(props) {
         </button>
       </div>
       
-      {mode === "login" && <LoginForm setCurrentUser={props.setCurrentUser}/>}
+      {/* {mode === "login" && <LoginForm setCurrentUser={props.setCurrentUser}/>} */}
+      {mode === "login" && <LoginCard setCurrentUser={props.setCurrentUser}/>}
       {mode === "signup" && <SignupForm setCurrentUser={props.setCurrentUser}/>}
 
     </div>
@@ -86,5 +102,67 @@ function SignupForm(props) {
           </li>
         </div>
       </form>
+  );
+}
+
+
+
+function LoginCard(props) {
+  let [email, setEmail] = React.useState('');
+  let [password, setPassword] = React.useState('');
+
+  const handleSubmit = async () => {
+    await BackendActor.handleLogin(email, password, props.setCurrentUser);
+  }
+
+  return (
+    <Flex
+      minH={'50vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              handleSubmit();
+            }}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input 
+                type="email" 
+                onChange={(event) => {setEmail(event.target.value)}}/>
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <Input 
+                type="password" 
+                onChange={(event) => (setPassword(event.target.value))}/>
+            </FormControl>
+            <Stack spacing={10} mt={5}>
+              <Button
+                bg={'blue.400'}
+                color={'black'}
+                _hover={{
+                  bg: 'blue.500',
+                  color: 'white',
+                }}
+                type="submit">
+                Sign in
+              </Button>
+            </Stack>
+            </form>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 }
