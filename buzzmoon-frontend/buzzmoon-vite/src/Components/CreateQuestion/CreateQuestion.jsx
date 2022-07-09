770f55193ead5aa4c53478d7c1561568551fedd5
Henry Cafaro <hjcafaro@fb.com>
2022-07-09 15:50:51 -0700

    rewrote create game view in chakra, removed css

--- buzzmoon-frontend/buzzmoon-vite/src/Components/CreateQuestion/CreateQuestion.jsx
+++ buzzmoon-frontend/buzzmoon-vite/src/Components/CreateQuestion/CreateQuestion.jsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
-import './CreateQuestion.css';
+
+import { Box, IconButton, Text, VStack, Flex, Icon, Center, Textarea, HStack} from '@chakra-ui/react';
+import {AiOutlineClose} from 'react-icons/ai'
+
 
 export default function CreateQuestion(props) {
   const [questionAudio, setQuestionAudio] = React.useState();
@@ -21,6 +24,67 @@ export default function CreateQuestion(props) {
       { ...props.question, audioFile: (e.target.files[0]) },
     );
   };
+
+  return (
+    <VStack w={'100%'} 
+      bg={'gray.50'} 
+      shadow={'md'}
+      spacing={'5'} 
+      padding={'3'} 
+      border={'1px solid black'}
+      borderRadius={'md'} >
+      <Flex w={'100%'} justify={'space-between'} align={'center'}>
+        <Text fontWeight={'bold'} >Question #
+          {props.question.number}
+        </Text>
+        <HStack spacing={'0'} maxW={'50%'}>
+          {questionAudio
+                && (playingMode === 'paused'
+                  ? (
+                    <IconButton
+                      fontSize={'lg'}
+                      variant={'outline'}
+                      onClick={() => {
+                        setPlayingMode('playing');
+                        questionAudio.play();
+                      }}
+                      icon={<i className="fa-solid fa-play"/>} />
+                  )
+                  : (
+                    <IconButton
+                      fontSize={'lg'}
+                      variant={'outline'}
+                      onClick={() => {
+                        setPlayingMode('paused');
+                        questionAudio.pause();
+                      }}
+                      icon={<i className="fa-solid fa-pause"/>} />
+                  )
+                )}
+        <input type="file" name="audio-file" accept=".mp3, .m4a" onChange={handleUploadFile} title="Choose an .mp3 or .m4a" />
+        </HStack>
+        <IconButton
+          variant={'ghost'}
+          type="button"
+          name="delete-question-button"
+          onClick={() => {
+            props.deleteQuestion(props.question.number);
+          }}
+          icon={<Icon fontSize={'32'} as={AiOutlineClose}></Icon>}/>
+      </Flex>
+      <Textarea
+          borderColor={'gray.500'}
+          placeholder="Enter answers as a comma separated list, i.e: gold, aurum, pyrite"
+          value={props.question.answers}
+          onChange={(e) => {
+            props.modifyQuestion(
+              props.question.number,
+              { ...props.question, answers: (e.target.value) },
+            );
+          }}
+        />
+    </VStack>
+  )
 
   return (
     <div className="create-question">
