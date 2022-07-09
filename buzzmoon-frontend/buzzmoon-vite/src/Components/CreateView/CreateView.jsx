5323091c0dd329a88acfb5ff570bbfc9fe840f4d
Henry Cafaro <hjcafaro@fb.com>
2022-07-09 00:36:34 -0700

    retooled create view with chakra, removed css

--- buzzmoon-frontend/buzzmoon-vite/src/Components/CreateView/CreateView.jsx
+++ buzzmoon-frontend/buzzmoon-vite/src/Components/CreateView/CreateView.jsx
@@ -2,31 +2,32 @@ import * as React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import './CreateView.css';
 
+import { VStack, Box, Heading, Flex, Center, Button, Text} from '@chakra-ui/react';
+
 import CreateGame from '../CreateGame/CreateGame';
 
 export default  function CreateView(props) {
   return (
-    <div className='create-view'>
       <Routes>
         <Route path="/" element={
-          <div className='create-options'>
-            <div className='game-list'>
-              <h2>Your Games</h2>
-              <div className='available-games-body'>
-                Nothing found
-              </div>
-            </div>
+          <Center>
+            <Box mt={'10'}>
+              <VStack spacing={'8'}>
+                <VStack boxShadow={'xl'} bg={'gray.200'} p={'5'} spacing={'5'} align={'start'}>
+                  <Heading size="md" borderBottom={'2px solid black'}>Your Created Games</Heading>
+                </VStack>
+                <Link to="/create/new">
+                  <Button leftIcon={<i className="fa-solid fa-plus"></i>} colorScheme={'green'}>
+                    Create New
+                  </Button>
+                </Link>
+              </VStack>
+            </Box>
 
-            <Link to="/create/new">
-              <button type="button" className='create-new-button'>
-                <i className="fa-solid fa-plus"></i> Create New
-              </button>
-            </Link>
-          </div>}>
+            
+          </Center>}>
         </Route>
         <Route path="/new" element={<CreateGame />}></Route>
       </Routes>
-      
-    </div>
   )
 }
