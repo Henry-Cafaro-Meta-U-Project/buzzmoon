cabf896dc8776498432d021a5ceddcb624838023
Henry Cafaro <hjcafaro@fb.com>
2022-07-19 14:04:26 -0700

    update styling of game list

--- buzzmoon-frontend/buzzmoon-vite/src/Components/GameList/GameList.jsx
+++ buzzmoon-frontend/buzzmoon-vite/src/Components/GameList/GameList.jsx
@@ -6,9 +6,9 @@ import { VStack, Heading, Spinner} from '@chakra-ui/react';
 
 export default function GameList({heading, games}) {
   return (
-    <VStack boxShadow={'xl'} bg={'gray.200'} p={'5'} spacing={'5'} mb={'5'} minW={'20%'} minH={'100%'}>
+    <VStack boxShadow={'2xl'} bg={'white'} p={'5'} spacing={'5'} mb={'5'} minW={'20%'} minH={'100%'}>
       <Heading size="md" borderBottom={'2px solid black'}>{heading}</Heading>
-      <VStack spacing={'1px'}>
+      <VStack spacing={'2'}>
         {games ?
           games.map((e) => (
             <GameListItem key={e.gameID} game={e}/>
