0daf2a030e43f51d480323f79218f0d676bc55d4
Henry Cafaro <hjcafaro@fb.com>
2022-07-20 13:35:02 -0700

    add details screen to head to head table in results page with popover

--- buzzmoon-frontend/buzzmoon-vite/src/Components/Results/Results.jsx
+++ buzzmoon-frontend/buzzmoon-vite/src/Components/Results/Results.jsx
@@ -3,9 +3,12 @@ import { useParams } from 'react-router-dom';
 import BackendActor from '../BackendActor/backend-actor';
 
 import { Spinner, Center, Tooltip, Heading, Table, Thead, Tbody, Tr, Th, Td, Icon,
-     TabList, Tabs, Tab, TabPanels, TabPanel, VStack, Box, HStack, Text} from '@chakra-ui/react';
-import {AiFillTrophy, AiOutlineQuestionCircle} from 'react-icons/ai'
+        TabList, Tabs, Tab, TabPanels, TabPanel, VStack, Box, HStack, Text, Button,
+        Popover, PopoverBody, PopoverContent, PopoverTrigger, PopoverArrow,
+        PopoverHeader, PopoverCloseButton} from '@chakra-ui/react';
+import {AiFillTrophy, AiOutlineQuestionCircle, AiOutlineCheck, AiOutlineClose, AiOutlineDash} from 'react-icons/ai'
 
+import { TiEquals } from "react-icons/ti";
 
 const formatConfig = {
   minimumFractionDigits: 3,
@@ -131,20 +134,60 @@ function StandardTable(props) {
 
 function HeadToHeadTable(props) {
   return (
-    <Table w={'100%'}variant={'striped'} maxW={'600px'}>
+    <Table w={'100%'} variant={'simple'} maxW={'600px'}>
       <Thead>
         <Tr>
           <Th>Player</Th>
           <Th>Record</Th>
           <Th>Win %</Th>
+          <Th></Th>
         </Tr>
       </Thead>
       <Tbody>
         {props.table.map((e, idx) => (
-          <Tr key={idx}>
+          <Tr key={idx} backgroundColor={((1+idx%2)) === 1 ? "gray.100" : "white"}>
             <Td>{trophyIcon(idx+1)} {e.name} </Td>
             <Td>{`${e.w}-${e.l}-${e.t}`}</Td>
             <Td isNumeric>{formatter.format(e.percentage)}</Td>
+            <Td>
+              <Popover >
+                <PopoverTrigger>
+                  <Button colorScheme={'blue'}>Details</Button>
+                </PopoverTrigger>
+                <PopoverContent w={'fit-content'}>
+                  <PopoverArrow />
+                  <PopoverCloseButton />
+                  <PopoverBody>
+                    <Table variant={'unstyled'} key={e.name}>
+                      <Thead>
+                        <Tr bg={'white'}>
+                          <Th></Th>
+                          <Th>Opponent</Th>
+                          <Th>Score</Th>
+                        </Tr>
+                      </Thead>
+                      <Tbody>
+                        {e.outcomes.map((o, idx) => (
+                          <Tr key={Math.random() + e.name + idx + o.oppName}>
+                            <Td>
+                              {o.res === "win" &&
+                                <Icon color={'green'} fontWeight={'2xl'} as={AiOutlineCheck}></Icon>}
+                              {o.res === "loss" &&
+                                <Icon color={'red'} fontWeight={'2xl'} as={AiOutlineClose}></Icon>}
+                              {o.res === "tie" &&
+                                <Icon fontWeight={'2xl'}  as={TiEquals}></Icon>}
+
+                            </Td>
+                            <Td>{o.oppName}</Td>
+                            <Td whiteSpace={'no-wrap'}>{`${o.score}-${o.oscore}`}</Td>
+                          </Tr>
+                        ))}
+                      </Tbody>
+                    </Table>
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
+            </Td>
           </Tr>)
         )}
       </Tbody>
