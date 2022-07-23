8fdfd3369fbfbb9b43bbf28e5841c44f72b57586
Henry Cafaro <hjcafaro@fb.com>
2022-07-23 14:42:34 -0700

    modify gamelistitem layout for end date

--- frontend/vite/src/Components/GameListItem/GameListItem.jsx
+++ frontend/vite/src/Components/GameListItem/GameListItem.jsx
@@ -3,7 +3,8 @@ import {
   useNavigate} from 'react-router-dom';
 
 
-import { VStack, IconButton, Flex, Icon, Text} from '@chakra-ui/react';
+import { VStack, IconButton, Flex, Icon, Text, Tooltip, Box} from '@chakra-ui/react';
+import { BsHourglassSplit } from "react-icons/bs";
 
 import {IoEnterOutline} from "react-icons/io5"
 
@@ -19,20 +20,25 @@ export default function GameListItem(props) {
       boxShadow={'sm'}>
     <Flex w={'100%'} justify={'space-between'} align={'start'} >
       <VStack align={'start'} spacing={'4px'}>
-        <VStack align={'start'} spacing={'0'}>
-        <Text mr={'5'} fontWeight={'bold'}>{props.game.title} </Text>
-        {props.game.endDate && <Text fontSize={'xs'}>ending {props.game.endDate.toLocaleString()}</Text>}
-        </VStack>
+        <Flex>
+          <Text fontWeight={'bold'}>{props.game.title} </Text>
+          {props.game.endDate && <Tooltip 
+            label={`open until ${props.game.endDate.toLocaleString()}`}
+            bg='gray.300' color='black'>
+            <Box ml={'2'} as={'span'}><Icon as={BsHourglassSplit}></Icon></Box>
+          </Tooltip>}
+        </Flex>
         <Text mr={'5'}>{props.game.authorName} </Text>
       </VStack>
-      <Flex justify={'end'}>
-      <IconButton
-        colorScheme={'white'}
-        color={'black'}
-        onClick={() => {
-          navigate(`/compete/${props.game.gameID}`);}}
-        icon={<Icon fontSize={'24px'} as={IoEnterOutline}></Icon>} />
-      </Flex>
+      <VStack align={'end'}>
+        <IconButton
+          colorScheme={'white'}
+          color={'black'}
+          onClick={() => {
+            navigate(`/compete/${props.game.gameID}`);}}
+          icon={<Icon fontSize={'24px'} as={IoEnterOutline}></Icon>} />
+        
+      </VStack>
     </Flex>
     </VStack>
   )
