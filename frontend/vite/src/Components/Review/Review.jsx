594a0208ae7a55fc5bfe865a4bbad2ef2c8d55d6
Henry Cafaro <hjcafaro@fb.com>
2022-07-24 16:46:31 -0700

    started setting up change board

--- frontend/vite/src/Components/Review/Review.jsx
+++ frontend/vite/src/Components/Review/Review.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import {BackendActor} from '../../Server/BackendActor/backend-actor';
 import ResultsEngine from '../../Logic/ResultsEngine';
 
-import { Spinner, Center, Select, Heading, Table, Thead, Tbody, Tr, Th, Td, Icon,
+import { Spinner, Center, Select, Heading, Flex, Icon,
         TabList, Tabs, Tab, TabPanels, TabPanel, VStack, Box, HStack, Text, Button,
         Popover, PopoverBody, PopoverContent, PopoverTrigger, PopoverArrow,
          PopoverCloseButton} from '@chakra-ui/react';
@@ -21,10 +21,9 @@ const formatter = new Intl.NumberFormat('en-US', formatConfig);
 export default function Results(props) {
   const {gameID} = useParams();
   const [gameData, setGameData] = React.useState();
-  console.log("🚀 ~ file: Review.jsx ~ line 24 ~ Results ~ gameData", gameData)
   const [results, setResults] = React.useState([]);
-  console.log("🚀 ~ file: Review.jsx ~ line 25 ~ Results ~ results", results)
   const [selectedQuestion, setSelectedQuestion] = React.useState(1);
+  const [changes, setChanges] = React.useState([]);
 
 
   React.useEffect(() => {
@@ -48,7 +47,8 @@ export default function Results(props) {
   }
 
   return (
-    <VStack mt={'20'} mx={'5%'} align={'start'}>
+    <Flex mt={'20'} mx={'5%'} direction={'row'} wrap={'wrap'} justify={'space-between'}>
+      <VStack align={'start'} width={{sm:"100%", md:"70%"}}>
         <VStack align={'start'}>
         <Heading>{gameData.title}</Heading>
         <Heading size={'sm'}>Review Answers</Heading>
@@ -67,6 +67,13 @@ export default function Results(props) {
         <AnswerReviewBoard gameData={gameData} questionNumber={parseInt(selectedQuestion)} results={results}/>
         </VStack>
       </VStack>
+      <VStack mt={{base:"0", sm:"10", md:"0"}} align={{sm:"start", md:"end"}} width={{sm:"100%", md:"30%"}}>
+      <VStack align={'start'} bg={'gray.200'} padding={'10px'} >
+        <Heading>Changes</Heading>
+        {changes.length === 0 ? <Text>nothing yet</Text> : null}
+      </VStack>
+      </VStack>
+    </Flex>
   )
 }
 
@@ -79,29 +86,28 @@ function AnswerReviewBoard(props) {
   console.log("🚀 ~ file: Review.jsx ~ line 77 ~ AnswerReviewBoard ~ givenAnswers", givenAnswers)
 
   return (
-    <VStack 
-      paddingTop={'10px'}
-      marginTop={'10px'}
-      borderTop={'1px solid gray'} 
-      w={'100%'} align={'start'}>
-      <Heading size={'md'}>Question #{props.questionNumber}</Heading>
-      <Heading size={'md'}>Initial Answers: {initialAnswers.join(", ")}</Heading>
-      <VStack align={'start'}>
-        {givenAnswers.map((a) => (
-          <VStack 
-            w={'100%'}
-            align={'start'} 
-            my={'10px'} 
-            border={'1px solid black'}
-            padding={'5px'}
-            bg={'gray.300'}>
-            <Heading size={'xs'}>Answer: "{truncateString(a.givenAnswer, 50)}"</Heading>
-            <Heading size={'xs'}>Ruling: {a.isCorrect ? "Correct" : "Incorrect"}</Heading>
-          </VStack>
-        ))}
+      <VStack 
+        paddingTop={'10px'}
+        marginTop={'10px'}
+        borderTop={'1px solid gray'} 
+        w={'100%'} align={'start'}>
+        <Heading size={'md'}>Question #{props.questionNumber}</Heading>
+        <Heading size={'sm'}>Initial Answers: {initialAnswers.join(", ")}</Heading>
+        <VStack align={'start'} spacing={'10px'}>
+          {givenAnswers.map((a) => (
+            <VStack 
+              w={'100%'}
+              align={'start'} 
+              border={'1px solid black'}
+              padding={'5px'}
+              bg={'gray.300'}>
+              <Heading size={'xs'}>Answer: "{truncateString(a.givenAnswer, 50)}"</Heading>
+              <Heading size={'xs'}>Ruling: {a.isCorrect ? "Correct" : "Incorrect"}</Heading>
+            </VStack>
+          ))}
 
+        </VStack>
       </VStack>
-    </VStack>
   );
 }
 
